d89f6e142c74be6a7a0a49e7ffb1fe8a81d75148
jwty <24990977+jwty@users.noreply.github.com>
2018-08-09 21:56:59 +0200

    stylistic changes, checksum now calculated in chunks

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ from werkzeug.security import check_password_hash
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
-def calldb(query):
+def call_db(query):
     database = sqlite3.connect(app.config['DATABASE'])
     cursor = database.cursor()
     result = cursor.execute(query).fetchone()
@@ -20,20 +20,28 @@ def calldb(query):
     database.close()
     return result
 
-def writetodb(originalfilename, newfilename, checksum):
-    query = 'INSERT INTO files (originalfilename, newfilename, checksum) VALUES ("{}", "{}", "{}")'.format(originalfilename, newfilename, checksum)
-    calldb(query)
+def write_to_db(original_filename, new_filename, checksum):
+    query = 'INSERT INTO files (original_filename, new_filename, checksum) VALUES ("{}", "{}", "{}")'.format(original_filename, new_filename, checksum)
+    call_db(query)
 
-def checkduplicate(checksum):
-    query = 'SELECT * FROM files WHERE checksum="{}"'.format(checksum)
-    result = calldb(query)
-    if result is None:
-        return False #file unique
-    return True #duplicate
+def calc_md5(file_up):
+    md5_obj = md5()
+    chunk_size = app.config['CHUNK_SIZE']
+    file_buffer = file_up.read(chunk_size)
+    while len(file_buffer) > 0:
+        md5_obj.update(file_buffer)
+        file_buffer = file_up.read(chunk_size)
+    file_up.seek(0)
+    return md5_obj.hexdigest()
 
-def getfilebychecksum(checksum):
+def check_duplicate(checksum):
     query = 'SELECT * FROM files WHERE checksum="{}"'.format(checksum)
-    filename = calldb(query)[2]
+    result = call_db(query)
+    return result
+
+def get_file_by_checksum(checksum):
+    query = 'SELECT * FROM files WHERE checksum="{}"'.format(checksum)
+    filename = call_db(query)[2]
     return filename
 
 @app.route('/')
@@ -42,22 +50,21 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['file_up']
+    file_up = request.files['file_up']
     secret = request.form['secret']
     if check_password_hash(app.config['AUTH_SECRET'], secret):
-        md5s = md5(file.read()).hexdigest()
-        file.seek(0)
-        if  checkduplicate(md5s):
-            newfilename = getfilebychecksum(md5s)
+        md5_sum = calc_md5(file_up)
+        if check_duplicate(md5_sum):
+            new_filename = get_file_by_checksum(md5_sum)
         else:
-            ext = os.path.splitext(file.filename)[1]
-            newfilename = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8)) + ext
-            filepath = os.path.join(app.config['UPLOAD_DIR'], newfilename)
-            file.save(filepath)
-            writetodb(file.filename, newfilename, md5s)
+            ext = os.path.splitext(file_up.filename)[1]
+            new_filename = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8)) + ext
+            file_path = os.path.join(app.config['UPLOAD_DIR'], new_filename)
+            file_up.save(file_path)
+            write_to_db(file_up.filename, new_filename, md5_sum)
         if 'json' in request.args:
-            return jsonify(response='ok', file=file.filename, url='{}{}'.format(request.url_root, newfilename))
-        return '<title>Upload complete</title><i>%s</i> => <i><a href="/%s">%s</a></i>' % (file.filename, newfilename, newfilename)
+            return jsonify(response='ok', file=file_up.filename, url='{}{}'.format(request.url_root, new_filename))
+        return '<title>Upload complete</title><i>%s</i> => <i><a href="/%s">%s</a></i>' % (file_up.filename, new_filename, new_filename)
     else:
         return jsonify(response='wrong_password')
 
--- setup.py
+++ setup.py
@@ -12,22 +12,23 @@ try:
 except:
     raise
 
+chunksize = int(input('Chunk size used for calculating checksum in bytes (default: 4194304): ') or '4194304')
+
 dbname = str(input('Db file name (default: database.db): ') or 'database.db')
 if os.path.exists(dbname):
     raise Exception('{} already present'.format(dbname))
 database = sqlite3.connect(dbname)
 cursor = database.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, originalfilename TEXT, newfilename TEXT, checksum TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, original_filename TEXT, new_filename TEXT, checksum TEXT)')
 database.commit()
 database.close()
 
 secrethash = generate_password_hash(str(getpass('Secret key (default: secret): ')) or 'secret')
 
 print('Writing config to config.cfg')
-if os.path.exists('config.cfg'):
-    raise Exception('config.cfg already present')
-with open('config.cfg', 'a') as cfg:
+with open('config.cfg', 'w') as cfg:
     cfg.write('DATABASE="{}"'.format(dbname))
+    cfg.write('\nCHUNK_SIZE={}'.format(chunksize))
     cfg.write('\nUPLOAD_DIR="{}"'.format(uploaddir))
     cfg.write('\nAUTH_SECRET="{}"'.format(secrethash))
     cfg.write('\n')
