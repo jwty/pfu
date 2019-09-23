ddd12f7bf7e70c944f80d3504a87c3f6dd97b03e
jwty <24990977+jwty@users.noreply.github.com>
2019-09-23 21:32:12 +0200

    * added file expiry date (for future use) * keep field is not saved to db (not necessary)

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Python file uploader using Flask
 ## Requirements
 `flask` and `werkzeug`
 
-## Usage
+## Installation & usage
+### Setup
 Clone and run `setup.py` to set up a sqlite3 database, config file and uploads directory (make sure script has write permissions).
 
 Run `server.py` for [developement server](http://flask.pocoo.org/docs/1.0/server/) or use [one of deployement options](http://flask.pocoo.org/docs/0.12/deploying/#deployment),
@@ -13,6 +14,15 @@ for example `gunicorn`:
 $ gunicorn --bind 0.0.0.0:8000 server:app
 ```
 
+### Form fields
+* `file_up` - file to upload
+* `secret` (string) - secret key (as set in by `setup.py`)
+* `expire` (bool) - to set file expiry date (as for now does nothing but useful nonetheless)
+* `expire_date` (string) - file expiry date in YYYY-MM-DD format (cause html)
+* `expire_time` (string) - file expiry time in HH:MM time (cause html)
+* `keep` (bool) - whether to keep original file name or not (original file name is kept as first segment of newly generated file name, for example `test-wgownpqy.jpg`)
+
+### Usage
 Use a web interface (`localhost:5000` if using dev server) or send POST request manually:
 ```
 $ curl -X POST http://localhost:5000/upload -F 'file_up=@example.txt' -F 'secret=secret'
@@ -26,4 +36,21 @@ $ curl -X POST http://localhost:5000/upload?json -F 'file_up=@example.txt' -F 's
   "response": "ok", 
   "url": "http://localhost:5000/9q6w3z6d.txt"
 }
+```
+
+Using `requests` library:
+```
+import requests
+url = "http://127.0.0.1:5000/upload?json"
+
+data = {
+        'secret' : 'secret',
+        'expire' : True,
+        'expire_date' : '2019-10-10',
+        'expire_time' : '16:20',
+        'keep' : True,
+}
+files = { 'file_up' : open('test.jpg', 'rb') }
+
+r = requests.post(url, data=data, files=files)
 ```
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import os
 import random
 import string
 import sqlite3
+from datetime import datetime
 from time import time
 from hashlib import md5
 from flask import Flask, render_template, request, send_from_directory, jsonify
@@ -13,18 +14,14 @@ from werkzeug.security import check_password_hash
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
-def write_to_db(cursor, original_filename, new_filename, upload_date, keep, checksum):
-    query = 'INSERT INTO files (original_filename, new_filename, upload_date, keep, checksum) VALUES (?, ?, ?, ?, ?)'
-    cursor.execute(query, [original_filename, new_filename, upload_date, keep, checksum])
+def write_to_db(cursor, original_filename, new_filename, upload_date, expire_date, checksum):
+    query = 'INSERT INTO files (original_filename, new_filename, upload_date, expire_date, checksum) VALUES (?, ?, ?, ?, ?)'
+    cursor.execute(query, [original_filename, new_filename, upload_date, expire_date, checksum])
 
 def get_file_by_checksum(cursor, checksum):
     query = 'SELECT * FROM files WHERE checksum=?'
     result = cursor.execute(query, [checksum]).fetchone()
     return result
-
-def mark_keep(cursor, checksum):
-    query = 'UPDATE files SET keep = 1 WHERE checksum=?'
-    cursor.execute(query, [checksum])
 
 def calc_md5(file_up):
     md5_obj = md5()
@@ -38,7 +35,9 @@ def calc_md5(file_up):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    default_date = datetime.now().strftime("%Y-%m-%d")
+    default_time = datetime.now().strftime("%H:%M")
+    return render_template('index.html', default_date=default_date, default_time=default_time)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -52,9 +51,15 @@ def upload_file():
     md5_sum = calc_md5(file_up)
     database = sqlite3.connect(app.config['DATABASE'])
     cursor = database.cursor()
-    keep = int('keep' in request.form)
+    keep = 'keep' in request.form
+    expire = 'expire' in request.form
+    if expire:
+        expire_date = request.form['expire_date']
+        expire_time = request.form['expire_time']
+        expire_date = int(datetime.strptime(expire_date+expire_time, '%Y-%m-%d%H:%M').strftime('%s'))
+    else:
+        expire_date = None
     if get_file_by_checksum(cursor, md5_sum):
-        if keep: mark_keep(cursor, md5_sum)
         new_filename = get_file_by_checksum(cursor, md5_sum)[2]
     else:
         ext = os.path.splitext(file_up.filename)[1]
@@ -62,7 +67,7 @@ def upload_file():
         new_filename = '{}-{}{}'.format(os.path.splitext(file_up.filename)[0], random_string, ext) if keep else random_string + ext
         file_path = os.path.join(app.config['UPLOAD_DIR'], new_filename)
         file_up.save(file_path)
-        write_to_db(cursor, file_up.filename, new_filename, int(time()), keep, md5_sum)
+        write_to_db(cursor, file_up.filename, new_filename, int(time()), expire_date, md5_sum)
     database.commit()
     database.close()
     if 'json' in request.args:
