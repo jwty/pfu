25544aa33845df65ed16c9887f358bb0423fc793
jwty <24990977+jwty@users.noreply.github.com>
2026-01-23 00:09:54 +0100

    Add a custom Jinja filter for truncating filenames

--- pfu/utils.py
+++ pfu/utils.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 class Messages:
@@ -10,3 +11,15 @@ def format_datetime(timestamp):
         return timestamp
     dt = datetime.fromtimestamp(timestamp).astimezone()
     return dt.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+
+def truncate_filename(filename, length=20):
+    if len(filename) <= length:
+        return filename
+    end = '(...)'
+    filename_root, filename_ext = os.path.splitext(filename)
+    truncate_to = length - len(filename_ext) - len(end)
+    # If the extension is longer than length, or no space for root, truncate whole string (should be rare)
+    if truncate_to <= 0 or len(filename_ext) >= length:
+        return filename[:length-len(end)] + end
+    return filename_root[:truncate_to] + end + filename_ext
