4fdab992e0f145ad0df1980c5ff8f74c14ecc856
jwty <24990977+jwty@users.noreply.github.com>
2026-01-26 02:02:48 +0100

    Add a file upload API route, add reusable file saving function

--- pfu/routes_api.py
+++ pfu/routes_api.py
@@ -1,24 +1,10 @@
 from flask import Blueprint, current_app, request
 from werkzeug.security import check_password_hash
 from pfu.db import get_file_by_filename, delete_by_filename
-from pfu.utils import prepare_file_details
+from pfu.utils import prepare_file_details, save_file
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
-error_404 = {'status': 'error', 'message': 'Not found'}
-error_401 = {'status': 'error', 'message': 'Unauthorized'}
-error_405 = {'status': 'error', 'message': 'Method not allowed'}
-
-
-# TODO: These are a mess; serve different error when prefix is /api
-@api.errorhandler(404)
-def not_found(error):
-    return error_404, 404
-
-
-@api.errorhandler(405)
-def method_not_allowed(error):
-    return error_405, 405
 
 
 @api.before_request
@@ -26,14 +12,14 @@ def before_request():
     api_secret = current_app.config['AUTH_SECRET']
     request_secret = request.headers.get('X-Auth-Secret')
     if not request_secret or not check_password_hash(api_secret, request_secret):
-        return error_401, 401
+        return {'status': 'error', 'message': 'Unauthorized'}, 401
 
 
 @api.get('/file/<filename>')
 def details(filename):
     file = get_file_by_filename(filename)
     if not file:
-        return error_404, 404
+        return {'status': 'error', 'message': 'Not found'}, 404
     file_details = prepare_file_details(current_app, request, file)
     return {'status': 'success', 'data': file_details}
 
@@ -41,9 +27,21 @@ def details(filename):
 @api.delete('/file/<filename>')
 def delete(filename):
     if not get_file_by_filename(filename):
-        return error_404, 404
+        return {'status': 'error', 'message': 'Not found'}, 404
     try:
         delete_by_filename(filename)
     except Exception as e:
         return {'status': 'error', 'message': str(e)}, 500
-    return {'status': 'success', 'message': 'File deleted successfully'}
+    return {'status': 'success'}
+    
+
+@api.post('/upload')
+def upload():
+    file = request.files.get('file')
+    if not file:
+        return {'status': 'error', 'message': 'No file provided'}, 400
+    keep_filename = 'keep_filename' in request.form
+    expire_timestamp = request.form.get('expire', None, int)
+    description = request.form.get('description')
+    status, file_data = save_file(file, keep_filename, expire_timestamp, description)
+    return {'status': status, 'data': file_data}
--- pfu/utils.py
+++ pfu/utils.py
@@ -1,5 +1,10 @@
 import os
+from flask import current_app, request
 from datetime import datetime
+from werkzeug.utils import secure_filename
+from secrets import token_urlsafe
+from time import time
+from pfu.db import calc_md5, get_file_by_checksum, add_file_to_db, get_file_by_filename 
 
 class Messages:
     DELETE_CONFIRM = 'File <a href="{file_url}">{filename}</a> <a href="{details_url}">(details)</a> will be deleted. This action cannot be undone. Type in your secret key to confirm:'
@@ -18,6 +23,7 @@ def prepare_file_details(current_app, request, file_data):
     file_prefix = current_app.config['FILE_URL_PREFIX']
     file_url = f'{base_url}{file_prefix}{file_data['filename']}'
     file_details_dict = {
+        'filename': file_data['filename'],
         'original_filename': file_data['original_filename'],
         'size': file_data['size'],
         'description': file_data['description'],
@@ -27,3 +33,25 @@ def prepare_file_details(current_app, request, file_data):
         'expire_date': file_data['expire_date']
     }
     return file_details_dict
+
+
+def save_file(file, keep_filename=False, expire_timestamp=None, description=None):
+    md5_sum = calc_md5(file)
+    if existing_file := get_file_by_checksum(md5_sum):
+        return 'file_exists', prepare_file_details(current_app, request, existing_file)
+    filename = secure_filename(file.filename)
+    filename_root, filename_ext = os.path.splitext(filename)
+    new_filename_root = token_urlsafe(current_app.config['FILENAME_LENGTH'])
+    if keep_filename:
+        new_filename = f'{filename_root}-{new_filename_root}{filename_ext}'
+    else:
+        new_filename = f'{new_filename_root}{filename_ext}'
+    file_path = os.path.join(current_app.config['UPLOAD_DIR'], new_filename)
+    try:
+        file.save(file_path)
+    except Exception as e:
+        return 'error', str(e)
+    file_size = os.stat(file_path).st_size
+    add_file_to_db(new_filename, file.filename, description, md5_sum, int(time()), expire_timestamp, file_size)
+    file_data = get_file_by_filename(new_filename)
+    return 'success', prepare_file_details(current_app, request, file_data)
